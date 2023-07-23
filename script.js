7a32fef0dc1748f42f5ffdb91e58185de820ec8a
Farid Hashimzada <hashimzadafarid@gmail.com>
2023-07-23 15:26:42 -0700

    language dropdown complete

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
-//owl.js
+//language Dropdown start
+
+const lang = document.querySelector('#lang')
+const langDrop = document.querySelector('#langDrop')
+
+lang.addEventListener('click', () => {
+    if (langDrop.style.display === 'none') {
+        langDrop.style.display = 'flex';
+    } else {
+        langDrop.style.display = 'none';
+    }
+});
+
+
+
+
+
+
+
+
+
+//OWL.JS
 
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
         },
-        600:{
-            items:3
+        600: {
+            items: 3
         },
-        1000:{
-            items:4
+        1000: {
+            items: 4
         }
     }
 })
